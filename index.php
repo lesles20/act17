b0e3e22501f9b5c0c519f97f03dcec8d741c25a7
Les Salas <>
2023-03-29 14:07:39 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+  <head>
+    <title>Datos</title>
+  </head>
+  <body>
+    <?php
+       $nombre = "Lesly Nataly Salas Rivera";
+       $calle = "calle Gavilan";
+       $numero = "1874";
+       $colonia = "zaragoza";
+       $municipio = "Saltillo coahuila";
+       $telefono = "844 123 456";
+
+       echo "¿cual es tu nombre?: $nombre  ";
+        ?>
+<br>
+<?php
+       echo "¿en que calle vives?: $calle ";
+        ?>
+<br>
+<?php
+       echo "¿en que colonia vives?: $colonia ";
+        ?>
+<br>
+<?php
+       echo " el numero es: $numero ";
+        ?>
+<br>
+<?php
+       echo " es en el municipio de: $municipio  ";
+       ?>
+<br>
+<?php
+       echo " si es que te pierdes llamame y te indico:$telefono";
+    ?>
+  </body>
+</html>
